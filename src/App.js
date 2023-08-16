49d9cf8acac5c7df6e648f634419a9d9546d5c83
zaputlah <reza.rr889@gmail.com>
2023-08-16 11:55:28 +0700

    update qoute

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  // Fungsi untuk mengambil kutipan acak dari API
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('https://api.quotable.io/random');
+      const data = response.data;
+      setQuote(data.content);
+      setAuthor(data.author);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []); // Menggunakan useEffect untuk mengambil kutipan saat komponen dimuat
+
+  // Fungsi untuk mengganti kutipan
+  const handleNewQuote = () => {
+    fetchQuote();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Quote Generator</h1>
+      <div className="quote-container">
+        <blockquote>
+          <p>{quote}</p>
+          <cite>- {author}</cite>
+        </blockquote>
+        <button onClick={handleNewQuote}>New Quote</button>
+      </div>
     </div>
   );
 }
